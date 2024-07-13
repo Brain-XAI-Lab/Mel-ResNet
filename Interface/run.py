37b52bc1bd15754dea0b0ccc4b01465e1ea7feb5
임도현 <94499717+LimDoHyeon@users.noreply.github.com>
2024-07-13 22:18:48 +0900

    [Feat]run.py
    #4

--- Interface/run.py
+++ Interface/run.py
@@ -10,53 +10,42 @@ The main function is as follows:
   - After performing this process three times, print the words stored in the buffer at once.
 """
 
-
-import numpy as np
-import mne
-import torch
-import torch.nn as nn
-import os
 from utils import *
-from Preprocessing.temp_preprocessing import preprocess_audio  # 전처리 함수를 정의한 파일에서 임포트
+import librosa
 
 
 def main():
-    # eeg_file = '/Users/imdohyeon/Library/CloudStorage/GoogleDrive-dhlim1598@gmail.com/공유 드라이브/4N_PKNU/BXAI/BMI/Mel-ResNet/EEG'  # not completed yet
-    # 테스트를 위해 일시적으로 EEG 데이터가 아닌 음성 데이터 사용
-    eeg_file = '/Users/imdohyeon/Library/CloudStorage/GoogleDrive-dhlim1598@gmail.com/공유 드라이브/4N_PKNU/BXAI/BMI/Mel-ResNet/Voice/Raw'
-    model_path = '/Users/imdohyeon/Documents/PythonWorkspace/Mel-ResNet/Model/'  # not completed yet
+    eeg_file = '/Users/imdohyeon/Library/CloudStorage/GoogleDrive-dhlim1598@gmail.com/공유 드라이브/4N_PKNU/BXAI/BMI/Mel-ResNet/Voice/Raw/Call/call1.wav'
+    model_path = '/Users/imdohyeon/Documents/PythonWorkspace/Mel-ResNet/Model/model_sample.pth'  # not completed yet
 
-    # EEG 데이터 로드
-    eeg_data = load_eeg_data(eeg_file)
-
-    # 윈도우 크기 및 이동 간격 설정
-    window_size = 1000  # 예시로 윈도우 크기를 설정
-    step_size = 500  # 예시로 윈도우 이동 간격을 설정
-
-    # 학습된 모델 로드
+    # EEG 데이터 및 모델 로드 (EEG preprocessing 완성 전까지는 wav 파일로 대체)
+    sampling_rate = 44100
+    data = librosa.load(eeg_file, sr=sampling_rate)
     model = load_model(model_path)
 
     # 버퍼 초기화
-    buffer = []
+    output_buffer = []
+    data_length = len(data)
 
-    # EEG 데이터 시퀀스를 윈도우로 순회
-    num_windows = (len(eeg_data) - window_size) // step_size + 1
-    for i in range(num_windows):
-        start = i * step_size
+    # EEG 데이터 시퀀스를 윈도우로 순회하며 분류
+    window_size = int(data_length * 0.4)
+    step_size = int((data_length - window_size) / 2)
+    start = 0
+
+    # 윈도우 순회 시작
+    for _ in range(3):
         end = start + window_size
-        window_data = eeg_data[:, start:end]
+        sliced_data = np.array(data[start:end])
 
-        # 데이터 전처리 (구체적인 전처리 내용은 preprocessing.py에서 수행)
-        mel_spectrogram = preprocess_window(window_data)
+        # 전처리 및 예측
+        output = predict(model, sliced_data, sampling_rate, device='cpu')
 
-        # ResNet 모델을 사용하여 Mel-spectrogram 분류
-        predicted_class = classify_spectrogram(model, mel_spectrogram)
-
-        # 버퍼에 결과 저장
-        buffer.append(predicted_class)
+        # 결과값 버퍼에 저장 후 다음 윈도우로
+        output_buffer.append(output)
+        start += step_size
 
     # 최종 결과 출력
-    print("Predicted classes for each window:", buffer)
+    print("Predicted classes for each window:", output_buffer)
 
 
 if __name__ == '__main__':
